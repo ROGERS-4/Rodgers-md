4178645b9b69adea9ce4ceb5e4c2a5c48fddfe90
ROGERS-4 <rogersonyango87@gmail.com>
2025-12-28 21:00:43 +0300

    Update and rename config. js to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  botName: "RODGERS MD",
+  channel: "https://whatsapp.com/channel/0029VbBR3ib3LdQQlEG3vd1x",
+  owner: ["+254755660053"], // owner numbers
+  prefixes: [".", "!", "¡"],
+  mode: "public" // public/private
+};
